783939293603ed8d47949f2780c0e32aadb76311
Pallav Singh <pshy3@umsystem.edu>
2023-04-28 16:06:02 -0500

    123

--- PCA/PCA.R
+++ PCA/PCA.R
@@ -11,8 +11,8 @@ pca_res <- prcomp(df2, center = TRUE, retx = TRUE, scale. = FALSE)
 inf <- info[-c(1:5,15:18),]
 inf <- info
 autoplot(pca_res, data = inf, colour = 'strain',size = 3, show.legend = FALSE) +
-  scale_fill_brewer(palette="Dark2")+ 
+  scale_fill_brewer(palette="Dark2")+
   #geom_point(data = inf, colour = info$?..strain, size = 3)+
-  geom_label_repel(aes(label = inf$ï..sample,color = inf$group),  show.legend = FALSE , nudge_y = 0.02, label.padding = 0.5, max.time = 2, max.overlaps = 20, force_pull = 2) + 
+  geom_label_repel(aes(label = inf$ï..sample,color = inf$group),  show.legend = FALSE , nudge_y = 0.02, label.padding = 0.5, max.time = 2, max.overlaps = 20, force_pull = 2) +
   theme(legend.title = element_text(size = 10), legend.text=element_text(size=15))
 
