03445ac3e37021000d89b8759d6cc208dff64856
Pallav Singh <pshy3@umsystem.edu>
2023-12-03 14:59:09 -0600

    123

--- rna-seq.py
+++ rna-seq.py
@@ -13,17 +13,39 @@ config.read('config.ini')
 substring = config.get('General', 'Substring')
 threads = config.get('General', 'Threads')
 reference_link = config.get('General', 'Reference_Link')
+output_folder = config.get('General', 'Output_Folder')
+
+def folder_structure():
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        
+    if not os.path.exists(output_folder+"/trimmed_files/"):
+        os.makedirs(output_folder+"/trimmed_files/")
+        
+    if not os.path.exists(output_folder+"/trimmed_files/trim_logs/"):
+        os.makedirs(output_folder+"/trimmed_files/trim_logs/")
+    
+    if not os.path.exists(output_folder+"/sam_output_cufflinks/"):
+        os.makedirs(output_folder+"/sam_output_cufflinks/")
+        
+    if not os.path.exists(output_folder+"/sam_output_cufflinks/align_logs/"):
+        os.makedirs(output_folder+"/sam_output_cufflinks/align_logs/")
+        
+    if not os.path.exists(output_folder+"/cufflinks_output/"):
+        os.makedirs(output_folder+"/cufflinks_output/")
+        
+    if not os.path.exists(output_folder+"/cufflinks_output/cufflinks_logs/"):
+        os.makedirs(output_folder+"/cufflinks_output/cufflinks_logs/")
+        
+    if not os.path.exists(output_folder+"/cuffdiff_output/"):
+        os.makedirs(output_folder+"/cuffdiff_output/")
+    
+    if not os.path.exists(output_folder+"/reference/"):   
+        os.makedirs(output_folder+"/reference/")   
 
 def trim_files():
-
-    if not os.path.exists('./trimmed_files/'):
-        os.makedirs('./trimmed_files/')
-
-    if not os.path.exists('./trimmed_files/trim_logs/'):
-        os.makedirs('./trimmed_files/trim_logs/')
-
     for filenames in glob.glob(substring):
-        filenames = filenames.split("1_001.fastq.gz")[0]
+        filenames = filenames.split("")[0]
         print(filenames)
         str1 = "(trim_galore --output_dir ./trimmed_files --paired "+filenames+"1_001.fastq.gz "+filenames+"2_001.fastq.gz) >& ./trimmed_files/trim_logs/log_"+filenames+"_trim.txt &"
         print(str1+"\n")
@@ -32,13 +54,7 @@ def trim_files():
 def sam_output():
     reference_file = "../reference/GRCm39"
     substring = "*_L004_R1_001_val_1.fq.gz"
-
-    if not os.path.exists('./sam_output_cufflinks/'):
-        os.makedirs('./sam_output_cufflinks/')
-
-    if not os.path.exists('./sam_output_cufflinks/align_logs/'):
-        os.makedirs('./sam_output_cufflinks/align_logs/')
-
+    
     for filenames in glob.glob(substring):
         filenames = filenames.split("_L004_R1_001_val_1.fq.gz")[0]
         print(filenames)
@@ -47,5 +63,6 @@ def sam_output():
         os.system (str1)
 
 # Call the functions
+folder_structure()
 trim_files()
 sam_output()
